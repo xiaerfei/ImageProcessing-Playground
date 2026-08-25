20baee67c8f8dd6545f40dad16201c4c1b114a14
erfeixia <erfeixia@gmail.com>
2026-08-25 21:51:53 +0800

    add ImageAlgorithm

---
+++ ImageAlgorithm/ImageAlgorithm/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ImageAlgorithm
+//
+//  Created by erfeixia on 25/08/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ImageAlgorithm/ImageAlgorithm/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ImageAlgorithm
+//
+//  Created by erfeixia on 25/08/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
