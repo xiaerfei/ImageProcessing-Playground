8b4c9776ac1531b7ee34dc32fc7f34aaaae89c04
erfeixia <erfeixia@gmail.com>
2026-08-25 21:51:53 +0800

    add ImageAlgorithm

---
+++ ImageAlgorithm/ImageAlgorithm/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  ImageAlgorithm
+//
+//  Created by erfeixia on 25/08/2026.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
---
+++ ImageAlgorithm/ImageAlgorithm/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ImageAlgorithm
+//
+//  Created by erfeixia on 25/08/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
